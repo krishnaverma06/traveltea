1678e4f9f63b2a67ba31fd1b0f725dbf782b4deb
Krishna verma <147273269+krishnaverma06@users.noreply.github.com>
2026-07-05 00:43:29 +0530

    Initial Setup with Landing Page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LandingPage from "./pages/LandingPage.jsx";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
