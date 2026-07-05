adfe5ce5e09a403771dc6bcaaffa141220fe9496
Krishna_Sharma <sharmakrishna0177@gmail.com>
2026-07-06 04:05:43 +0530

    Added Trip Details Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import TripPlannerPage from "./pages/TripPlannerPage.jsx";
 import BudgetPage from "./pages/BudgetPage.jsx";
 import PreferencesPage from "./pages/PreferencesPage.jsx";
 import ResultsPage from "./pages/ResultsPage.jsx";
+import TripDetailsPage from "./pages/TripDetailsPage.jsx";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path="/plan/budget" element={<BudgetPage />} />
           <Route path="/plan/preferences" element = {<PreferencesPage />}/>
           <Route path="/plan/results" element = {<ResultsPage/>}/>
+          <Route path="/plan/details" element = {<TripDetailsPage/>}/>
         </Routes>
       </Router>
     </TripProvider>
